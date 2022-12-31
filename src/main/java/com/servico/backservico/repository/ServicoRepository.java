8cb7c88c51252afadde7f3766c9ef8bc49019ddb
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-12-31 17:12:33 -0300

    Criando uma anotação no respositpry para conseguir buscar apenas os servicos pendentes

--- src/main/java/com/servico/backservico/repository/ServicoRepository.java
+++ src/main/java/com/servico/backservico/repository/ServicoRepository.java
@@ -1,9 +1,14 @@
 package com.servico.backservico.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.servico.backservico.entity.Servico;
 
 public interface ServicoRepository extends JpaRepository<Servico, Long>{
-
+	
+	@Query("select s from Servico s where s.valorPago <> null and s.valorPago > 0")
+	List<Servico> buscarServicosPagamentoPendente();
 }
