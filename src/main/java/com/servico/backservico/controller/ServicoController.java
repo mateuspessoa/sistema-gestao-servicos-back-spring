612490340e623c564871ce1486c0d30f1c946a82
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-12-31 18:51:30 -0300

    Correção: No Controller trocar o @PathParam por @PathVariable

--- src/main/java/com/servico/backservico/controller/ServicoController.java
+++ src/main/java/com/servico/backservico/controller/ServicoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -50,7 +51,7 @@ public class ServicoController {
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> excluir(@PathParam("id") Long id){
+	public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
 		servicoService.excluir(id);
 		return ResponseEntity.ok().build();
 	}
